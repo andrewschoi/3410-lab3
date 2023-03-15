f1dc3bac4fe645729585e0584d3c9a38fdfe3e4c
andrew <asc269@cornell.edu>
2023-03-14 21:00:42 -0400

    Create process.c

---
+++ process.c
@@ -0,0 +1,18 @@
+#include "3140_concur.h"
+#include <stdlib.h>
+#include <MKL46Z4.h>
+
+struct process_state {
+
+}
+
+int process_create(void *f(void), n) {
+}
+
+void process_start(void) {
+
+}
+
+unsigned int * process_select(unsigned int * cursp) {
+
+}
