1168fb2be2b6ec03330e738b4cb09f8ec01b6726
andrew <asc269@cornell.edu>
2023-03-20 15:13:26 -0400

    Update test for test_2 - interleaving processes

--- test_2.c
+++ test_2.c
@@ -1,6 +1,7 @@
 #include "3140_concur.h"
 #include "utils.h"
 
+// process 1 is a longer process
 void p1(void) {
   for (int i = 0; i < 6; i ++) {
     delay();
@@ -8,10 +9,13 @@ void p1(void) {
   }
 }
 
+// process 2 is a shorter process 
 void p2(void) {
   for (int i = 0; i < 3; i ++) {
     delay();
     LEDGreen_Toggle();
+    delay();
+    LEDRed_Toggle();
   }
 }
 
